3dc93f33fa900a173aa93f4d03881feb168daf10
Rithsagea <rithsagea@gmail.com>
2019-12-10 11:41:55 -0500

    .

--- Skyblock/Analyzer/com/rithsagea/skyblock/AnalyzerWindow.java
+++ Skyblock/Analyzer/com/rithsagea/skyblock/AnalyzerWindow.java
@@ -1,5 +1,9 @@
 package com.rithsagea.skyblock;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -12,7 +16,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.stat.StatUtils;
+
 import com.rithsagea.skyblock.api.DatabaseConnection;
+import com.rithsagea.skyblock.api.Logger;
 import com.rithsagea.skyblock.api.datatypes.Datapoint;
 
 public class AnalyzerWindow {
@@ -68,8 +78,9 @@ public class AnalyzerWindow {
 		return new Timestamp(calendar.getTimeInMillis());
 	}
 	
-	//TODO make this generate an empty array for future rolling average
 	public static Datapoint[] rollingAverage(Datapoint[] data, Timestamp start, Timestamp end, long interval, long window, TimeUnit unit) {
+		Logger.log("Getting the rolling average of " + data.length + " datapoints.");
+		
 		List<Datapoint> list = Arrays.asList(data);
 		Deque<Datapoint> countingData = new ArrayDeque<Datapoint>(list);
 		Deque<Datapoint> currentData = new ArrayDeque<Datapoint>();
@@ -97,14 +108,20 @@ public class AnalyzerWindow {
 			double price = 0;
 			int amount = 0;
 			
+			//write data
+			//TODO remove outliers
 			if(!currentData.isEmpty()) {
-			
+				List<Datapoint> a = new ArrayList<Datapoint>();
 				Iterator<Datapoint> iterator = currentData.iterator();
 				while(iterator.hasNext()) {
+					a.add(iterator.next());
+					//remove outliers here
 					Datapoint point = iterator.next();
 					price += point.value;
 					amount += point.amount;
 				}
+				
+				//calculate stuff here
 			}
 			
 			storedData.add(new Datapoint(time, price, amount));
@@ -116,22 +133,64 @@ public class AnalyzerWindow {
 		return storedData.toArray(array);
 	}
 	
-	public static void main(String[] args) throws SQLException {
-		//Get Data
-		ResultSet auctions = db.runQuery("select end_time, price, amount from skyblock.auction_data where item_type = \"WISE_FRAGMENT\" order by end_time asc");
-		Datapoint[] data = resultsToData(auctions);
-//		printData(data);
-		
-		//Process Data
+	public static Datapoint[] processData(Datapoint[] data) {
 		Timestamp start = data[0].time;
 		Timestamp end = data[data.length - 1].time;
-
+		
 		start = truncateTime(start);
 		end = truncateTime(end);
 		
 		end.setTime(end.getTime() + TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
 		
-		data = rollingAverage(data, start, end, 10, 10, TimeUnit.MINUTES);
-		printData(data);
+		long interval = 5;
+		long window = 30;
+		TimeUnit unit = TimeUnit.MINUTES;
+		
+		return rollingAverage(data, start, end, interval, window, unit);
+	}
+	
+	public static Datapoint[] removeOutliers(Datapoint[] data, double z_limit) {
+		List<Datapoint> withoutOutliers = new ArrayList<Datapoint>();
+		//calculate mean
+		double[] values = new double[data.length];
+		for(int x = 0; x < data.length; x++) {
+			values[x] = data[x].value / data[x].amount;
+		}
+		
+		double mean = StatUtils.mean(values);
+		double variance = StatUtils.populationVariance(values, mean);
+		double sd = Math.sqrt(variance);
+		NormalDistribution nd = new NormalDistribution();
+		
+		for(int x = 0; x < values.length; x++) {
+			double stdscore = (values[x] - mean) / sd;
+			double sf = 1.0 - nd.cumulativeProbability(Math.abs(stdscore));
+			Logger.log("" + stdscore + " " + sf);
+		}
+		
+		Datapoint[] r = new Datapoint[withoutOutliers.size()];
+		return withoutOutliers.toArray(r);
+	}
+	
+	public static void main(String[] args) throws SQLException, IOException {
+		//Get Data
+		String itemType = "WISE_FRAGMENT";
+		
+		ResultSet auctions = db.runQuery("select end_time, price, amount from skyblock.auction_data where item_type = \"WISE_FRAGMENT\" order by end_time asc");
+		Datapoint[] data = resultsToData(auctions);
+		
+		//Process Data
+		Datapoint[] pd = processData(data);
+		
+		//Write Rolling Average to CSV
+		BufferedWriter writer = Files.newBufferedWriter(Paths.get("./" + itemType + ".csv"));
+		CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT
+				.withHeader("timestamp", "total", "amount"));
+		
+		for(int x = 0; x < pd.length; x++) {
+			printer.printRecord(pd[x].time, pd[x].value, pd[x].amount);
+		}
+		printer.flush();
+		printer.close();
 	}
 }
