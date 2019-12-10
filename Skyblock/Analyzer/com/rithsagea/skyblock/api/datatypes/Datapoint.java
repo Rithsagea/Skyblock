57a4a4409e6ce3687d772de3bf850ab41ebf308f
Rithsagea <rithsagea@gmail.com>
2019-12-09 22:51:53 -0500

    Changed type from short to int to allow for larger quantities

--- Skyblock/Analyzer/com/rithsagea/skyblock/api/datatypes/Datapoint.java
+++ Skyblock/Analyzer/com/rithsagea/skyblock/api/datatypes/Datapoint.java
@@ -6,9 +6,9 @@ public class Datapoint {
 	
 	public Timestamp time = null;
 	public double value = 0;
-	public short amount = 0;
+	public int amount = 0;
 	
-	public Datapoint(Timestamp time, double value, short amount) {
+	public Datapoint(Timestamp time, double value, int amount) {
 		this.time = time;
 		this.value = value;
 		this.amount = amount;
